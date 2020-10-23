c269d850e72cb4daca318869789a8216c0eec292
ACLAVERIA <andre.claveria@ynov.com>
2020-10-23 22:42:17 +0200

    clear inventory and modify

--- src/main/java/fr/ayust/Main.java
+++ src/main/java/fr/ayust/Main.java
@@ -2,6 +2,9 @@ package fr.ayust;
 
 import fr.ayust.command.ChooseCmd;
 import fr.ayust.command.Duel;
+import fr.ayust.listener.PlayerDeath;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -9,6 +12,9 @@ public class Main extends JavaPlugin {
    @Override
    public void onEnable() {
 
+    Duel_Manager duelmanager = new Duel_Manager;
+       PluginManager pm = Bukkit.getPluginManager();
+       pm.registerEvents(new PlayerDeath(), this);
         getLogger().info("[Duel] has been enable");
         getCommand("choose").setExecutor(new ChooseCmd());
         getCommand("duel").setExecutor(new Duel());
